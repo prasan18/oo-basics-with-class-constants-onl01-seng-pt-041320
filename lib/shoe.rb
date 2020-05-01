accbfb3de698f25d57ac97eb366550be0f7cc785
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-05-01 01:15:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,8 +13,7 @@ class Shoe
   end
 def brand = (brand)
   @brand = brand
-  
+  if !(BRANDS.include?(@brand))
+      BRANDS << @brand
   end
 end
-if !(BRANDS.include?(@brand))
-      BRANDS << @brand
