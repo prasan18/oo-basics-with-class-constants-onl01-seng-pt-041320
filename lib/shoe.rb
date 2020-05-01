61ea72ae4ce6de1dc7ba7b4f6dbee957611a7411
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-05-01 01:11:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
-  attr_accessor :color,  :material, :condition
-  attr_reader :brand, 
-  SIZES = []
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -11,8 +11,8 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-def size (size)
-    @size = size
-    SIZES << size
+def brand = (brand)
+    @brand = brand
+    BRANDS << brand
   end
 end
