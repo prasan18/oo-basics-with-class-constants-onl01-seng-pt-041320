429687374a8e4109b09b540b24a782eb5eb31613
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-05-01 01:14:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,7 +12,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 def brand  (brand)
-    @brand = brand
-    BRANDS << brand
+  BRANDS = brand
   end
 end
+if !(BRANDS.include?(@brand))
+      BRANDS << @brand
